6091dd0bce7c794a49ed9f978ca5ec83058f9024
Archie Baer <archie@abaer.dev>
2020-03-21 14:27:47 +0000

    Formatting

--- api/v1/posts.js
+++ api/v1/posts.js
@@ -4,12 +4,16 @@ const getUser = require("../../util/getUser");
 
 module.exports = async (req, res) => {
 	res.json({
-		posts: await Promise.all((await db.Post.findAll({
-			order: [["createdAt", "DESC"]],
-			limit: 30
-		})).map(async p => {
-			const author = await getUser(p.authorId);
-			return postCardData(p, author.username);
-		}))
+		posts: await Promise.all(
+			(
+				await db.Post.findAll({
+					order: [["createdAt", "DESC"]],
+					limit: 30
+				})
+			).map(async p => {
+				const author = await getUser(p.authorId);
+				return postCardData(p, author.username);
+			})
+		)
 	});
 };
