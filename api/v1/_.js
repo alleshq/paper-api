5fc67e70b5b52ae518371bfb6443e77f2bba0737
Archie Baer <archie@abaer.dev>
2020-03-21 17:36:32 +0000

    Removed CORS

--- api/v1/_.js
+++ api/v1/_.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const auth = require("../../util/auth");
-const cors = require("cors");
 
 const router = express.Router();
 router.get("/me", auth(), require("./me"));
-router.get("/user", auth(true), cors(), require("./user"));
+router.get("/user", auth(true), require("./user"));
 router.get("/posts", auth(), require("./posts"));
-router.get("/post/:username/:slug", auth(true), cors(), require("./post"));
+router.get("/post/:username/:slug", auth(true), require("./post"));
 router.post("/like/:username/:slug", auth(), require("./like"));
 router.post("/unlike/:username/:slug", auth(), require("./unlike"));
 router.post("/new/:slug", auth(), require("./postCreate"));
